c45860c89da7faa25405a24e27033b3b01fa6a70
Abhilash Tu <abhilashstorm@gmail.com>
2020-12-15 22:32:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const themes = {
+  "midnight-purple": {
+    title_color: "9745f5",
+    icon_color: "9f4bff",
+    text_color: "ffff00",
+    bg_color: "000000",
+  },
+};
+
+module.exports = themes;
